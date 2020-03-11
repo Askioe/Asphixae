0c68cfe0997aa11c715f38a9853ff67bd0f8fc6b
Askioe <58087709+Askioe@users.noreply.github.com>
2020-03-11 11:56:23 -0700

    Update Fun.py

--- Asphixae-Rewritten-master/cogs/Fun.py
+++ Asphixae-Rewritten-master/cogs/Fun.py
@@ -1,6 +1,9 @@
 import discord
+import random
 from discord.ext import commands
 from discord.ext.commands import has_permissions, MissingPermissions
+
+
 
 class Fun(commands.Cog):
     """Fun stuff"""
@@ -17,6 +20,14 @@ class Fun(commands.Cog):
     async def say(self, ctx, *, raw):
         """Tells the bot to say exactly what you want it to say(Requires Administrator)\nFormat: )say {context} """
         await ctx.send(raw)
+
+    @commands.command()
+    async def coinflip(self, ctx):
+        """Just a coinflip.\nFormat: )coinflip"""
+        sides = ["Heads", "Tails"]
+        choice = random.choice(sides)
+        await ctx.send(choice)
+
 
 def setup(bot):
     bot.add_cog(Fun(bot))
